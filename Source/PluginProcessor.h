948788c3a399ad86e086ed160ffe7a4b025a9682
alidante90-wq <ali.dante90@gmail.com>
2026-09-10 05:03:03 +0330

    Full processor: factory presets (8), ADSR, arp/seq params, macros, delay time/fb

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,36 +1,45 @@
 #pragma once
 #include <JuceHeader.h>
+#include <map>
 #include "Synth/SynthEngine.h"
 #include "FX/SimpleDelay.h"
 
-class SalekHightechAudioProcessor : public juce::AudioProcessor {
+class SalekHightechAudioProcessor : public juce::AudioProcessor
+{
 public:
     SalekHightechAudioProcessor();
     ~SalekHightechAudioProcessor() override = default;
-    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override {}
-    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
-    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
     const juce::String getName() const override { return JucePlugin_Name; }
     bool acceptsMidi() const override { return true; }
     bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
-    double getTailLengthSeconds() const override { return 0.0; }
-    int getNumPrograms() override { return 1; }
-    int getCurrentProgram() override { return 0; }
-    void setCurrentProgram(int) override {}
-    const juce::String getProgramName(int) override { return {}; }
-    void changeProgramName(int, const juce::String&) override {}
-    void getStateInformation(juce::MemoryBlock& destData) override;
-    void setStateInformation(const void* data, int sizeInBytes) override;
+    double getTailLengthSeconds() const override { return 2.0; }
+    int getNumPrograms() override { return (int) factoryPresets.size(); }
+    int getCurrentProgram() override { return currentProgram; }
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int, const juce::String&) override {}
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
     juce::AudioProcessorValueTreeState& getAPVTS() { return apvts; }
     salek::SynthEngine& getSynthEngine() { return synthEngine; }
+    void loadFactoryPreset (int index);
+    juce::StringArray getPresetNames() const;
 private:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    void applyParamsToEngine();
+    void initFactoryPresets();
     juce::AudioProcessorValueTreeState apvts;
     salek::SynthEngine synthEngine;
     salek::SimpleDelay delay;
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SalekHightechAudioProcessor)
+    struct Preset { juce::String name; std::map<juce::String, float> values; };
+    std::vector<Preset> factoryPresets;
+    int currentProgram = 0;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SalekHightechAudioProcessor)
 };
