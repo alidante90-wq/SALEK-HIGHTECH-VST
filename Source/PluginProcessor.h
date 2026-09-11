d584707e978c578ef78db8480ab7277ab4706d97
alidante90-wq <ali.dante90@gmail.com>
2026-09-11 05:21:15 +0330

    v0.6: audio-reactive 3D cyber grid + peak metering API

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <JuceHeader.h>
+#include <atomic>
 #include <map>
 #include "Synth/SynthEngine.h"
 #include "FX/SimpleDelay.h"
@@ -13,34 +14,43 @@ class SalekHightechAudioProcessor : public juce::AudioProcessor
 public:
     SalekHightechAudioProcessor();
     ~SalekHightechAudioProcessor() override = default;
+
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override {}
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
+
     const juce::String getName() const override { return JucePlugin_Name; }
     bool acceptsMidi() const override { return true; }
     bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
-    double getTailLengthSeconds() const override { return 4.0; }
+    double getTailLengthSeconds() const override { return 2.0; }
+
     int getNumPrograms() override { return (int) factoryPresets.size(); }
     int getCurrentProgram() override { return currentProgram; }
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
     void changeProgramName (int, const juce::String&) override {}
+
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+
     juce::AudioProcessorValueTreeState& getAPVTS() { return apvts; }
     juce::MidiKeyboardState& getKeyboardState() { return keyboardState; }
-    salek::SynthEngine& getSynthEngine() { return synthEngine; }
+    float getOutputPeak() const { return outputPeak.load(); }
     salek::StepSequencer& getStepSequencer() { return stepSequencer; }
-    void loadFactoryPreset (int index);
+
     juce::StringArray getPresetNames() const;
+
 private:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
     void applyParamsToEngine();
     void initFactoryPresets();
+    void loadFactoryPreset (int index);
+
     juce::AudioProcessorValueTreeState apvts;
     salek::SynthEngine synthEngine;
     salek::SimpleDelay delay;
@@ -49,8 +59,11 @@ private:
     salek::Arpeggiator arpeggiator;
     salek::StepSequencer stepSequencer;
     juce::MidiKeyboardState keyboardState;
-    struct Preset { juce::String name; std::map<juce::String, float> values; };
-    std::vector<Preset> factoryPresets;
+    std::atomic<float> outputPeak { 0.0f };
+
+    struct FactoryPreset { juce::String name; std::map<juce::String, float> values; };
+    std::vector<FactoryPreset> factoryPresets;
     int currentProgram = 0;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SalekHightechAudioProcessor)
 };
