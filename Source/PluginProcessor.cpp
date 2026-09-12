c8e6d9a9d2d08d63580972cd5a01198f0e7bc968
alidante90-wq <ali.dante90@gmail.com>
2026-09-12 20:39:48 +0330

    Restore processor: thin cpp includes full PluginProcessorBody.inl with presets + soft limiter

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -2,11 +2,4 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-SalekHightechAudioProcessor::SalekHightechAudioProcessor()
-    : AudioProcessor (BusesProperties()
-          .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-          .withOutput ("Output", juce::AudioChannelSet::stereo(), true)),
-      apvts (*this, nullptr, "PARAMETERS", createParameterLayout())
-{
-    initFactoryPresets();
-}
+#include "PluginProcessorBody.inl"
